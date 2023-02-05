d0ecc508955035e9c9442f06ad7c374423448395
Kirill <kir1llnikolaeff@yandex.by>
2023-02-05 15:37:25 +0000

    Fix string being sets (wtf)

--- main.py
+++ main.py
@@ -2,24 +2,24 @@ from fastapi import FastAPI, Path
 
 dummy_data = {
     1: {
-        "Title": {"Crime and Punishment"},
-        "Author": {"Fedor Dostoevsky"},
+        "Title": "Crime and Punishment",
+        "Author": "Fedor Dostoevsky",
         "Price:": {10.12}
     },
     2: {
-        "Title": {"Complete Poetry of Edgar Allan Poe"},
-        "Author": {"Edgar Allan Poe"},
-        "Price:": {4.99}
+        "Title": "Complete Poetry of Edgar Allan Poe",
+        "Author": "Edgar Allan Poe",
+        "Price:": 4.99
     },
     3: {
-        "Title": {"Pride and Prejustice"},
-        "Author": {"Jane Austen "},
-        "Price:": {5.99}
+        "Title": "Pride and Prejustice",
+        "Author": "Jane Austen",
+        "Price:": 5.99
     },
     4: {
-        "Title": {"To Kill a Mockingbird"},
-        "Author": {"Harper Lee "},
-        "Price:": {9.99}
+        "Title": "To Kill a Mockingbird",
+        "Author": "Harper Lee",
+        "Price:": 9.99
     }
 }
 
@@ -36,6 +36,12 @@ def root():
 def about():
     return {"info": "This API is exremely useful and cool"}
 
-@app.get("/catalog/{item_id}")
+@app.get("/catalog/id/{item_id}")
 def get_item_by_id(item_id: int = Path(None, description="An ID of the item in a catalog", ge=min_id, le=max_id)):
     return dummy_data[item_id]
+
+@app.get("/catalog/query")
+def get_item_by_author(name: str = None):
+    for item_id in dummy_data:
+        if dummy_data[item_id]["Author"] == name:
+            return dummy_data[item_id]
