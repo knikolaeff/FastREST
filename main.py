7956f4b589308a4ba236692bba49c067e8565ffd
Kirill <kir1llnikolaeff@yandex.by>
2023-02-05 13:53:51 +0000

    Add some dummy text and get_by_id function

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 from fastapi import FastAPI
 
+dummy_data = {
+    1: {
+        "Title": {"Crime and Punishment"},
+        "Author": {"Fedor Dostoevsky"},
+        "Price:": {10.12}
+    },
+    2: {
+        "Title": {"Complete Poetry of Edgar Allan Poe"},
+        "Author": {"Edgar Allan Poe"},
+        "Price:": {4.99}
+    },
+    3: {
+        "Title": {"Pride and Prejustice"},
+        "Author": {"Jane Austen "},
+        "Price:": {5.99}
+    },
+    4: {
+        "Title": {"To Kill a Mockingbird"},
+        "Author": {"Harper Lee "},
+        "Price:": {9.99}
+    }
+}
+
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Hello World!"}
+
+@app.get("/about")
+def about():
+    return {"info": "This API is exremely useful and cool"}
+
+@app.get("/catalog/{item_id}")
+def get_item_by_id(item_id: int):
+    return dummy_data[item_id]
