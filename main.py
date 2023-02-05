b5bc5fe589603f24941fedcf022293402fafd384
Kirill <kir1llnikolaeff@yandex.by>
2023-02-05 18:54:54 +0000

    Fixed one last name

--- main.py
+++ main.py
@@ -11,27 +11,10 @@ class Book(BaseModel):
 
 
 dummy_data = {
-    # 1: {
-    #     "Title": "Crime and Punishment",
-    #     "Author": "Fedor Dostoefieldsky",
-    #     "Price:": 10.12,
-    # },
-    # 2: {
-    #     "Title": "Complete Poetry of Edgar Allan Poe",
-    #     "Author": "Edgar Allan Poe",
-    #     "Price:": 4.99,
-    # },
-    # 3: {
-    #     "Title": "Pride and Prejustice",
-    #     "Author": "Jane Austen",
-    #     "Price:": 5.99
-    # },
-
-    # 4: {
-    #     "Title": "To Kill a Mockingbird",
-    #     "Author": "Harper Lee",
-    #     "Price:": 9.99
-    # },
+    1: Book(title="Crime and Punishment", author="Fedor Dostoevsky", price=10.12),
+    2: Book(title="Complete Poetry of Edgar Allan Poe", author="Edgar Allan Poe", price=4.99),
+    3: Book(title="Pride and Prejustice", author="Jane Austen", price=5.99),
+    4: Book(title="To Kill a Mockingbird", author="Harper Lee", price=9.99)
 }
 
 app = FastAPI()
@@ -54,11 +37,7 @@ async def get_all_items():
 
 @app.get("/catalog/id/{item_id}")
 async def get_item_by_id(
-    item_id: int = Path(
-        None,
-        description="An ID of the item in a catalog",
-        gt=0
-    )
+    item_id: int = Path(None, description="An ID of the item in a catalog", gt=0)
 ):
     return dummy_data[item_id]
 
@@ -72,9 +51,7 @@ async def get_item_by_author(name: Optional[str] = None):
 
 
 @app.post("/catalog/new/{item_id}")
-async def create_new_book(
-    book: Book, item_id: int = Path(None, gt=0)
-):
+async def create_new_book(book: Book, item_id: int = Path(None, gt=0)):
     if item_id not in dummy_data:
         dummy_data[item_id] = book
     else:
